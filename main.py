7337fcc7cab348a467d033f3c19f83365724d79d
imonkfcwifi <nohyungkorea@gmail.com>
2023-01-09 00:35:55 +0900

    tuples

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 days_of_week = ["Mon" ,"Tue", "Wed", "Thu" , "Fri"]
 
-print(days_of_week)
+print(days_of_week.count("Mon"))
 # list\\
 
 name = "roh"
 
 print(name.upper)
 # .을 붙이면 뒤에 나오는 function은 함수들 method다
-# method는 데이터 뒤에 결합된 function이다
+# method는 데이터에 결합된 function이다
 # print(), random()<-와 같은 것들은 function// name. <- 에 들어가는 기능들은 method
+
+numbers = ("1","2","3")
+
+# tuples 는 불변성을 가진다. method를 추가하려고해도 추가할게 많이 없음
